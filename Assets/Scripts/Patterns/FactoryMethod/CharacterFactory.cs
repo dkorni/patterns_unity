8b69d2bdd8d125159a5f40da07e10062a14d7adf
Kornisoft <den.creativ.kor@gmail.com>
2020-05-01 01:14:01 +0300

    Factory method init

---
+++ Assets/Scripts/Patterns/FactoryMethod/CharacterFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterFactory
+{
+    public abstract ICharacter CreateCharacter();
+
+    public void SpawnCharacter(Vector3 point)
+    {
+        var character = CreateCharacter();
+        character.Walk();
+    }
+}
